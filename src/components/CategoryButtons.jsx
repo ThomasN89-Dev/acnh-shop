bb7df6ac8bd125396dcab058ddc0b6757660b569
t.neroni <t.neroni@reply.it>
2026-03-12 12:19:40 +0100

    fixed buttons visualization

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -18,17 +18,17 @@ const ButtonCategories = [
 
 function CategoryButtons({ setCategory, setLoading }) {
   return (
-    <div className="flex justify-around w-full px-4 mb-3">
+    <div className="flex justify-around w-full px-4 mb-3 gap-2">
       {ButtonCategories.map((button) => {
         return (
           <button
-            className="border p-2 bg-cyan-300 rounded-2xl"
+            className="border p-2 bg-cyan-300 rounded-2xl flex-1 "
             key={button.value}
             onClick={() => {
               (setCategory(button.value), setLoading(true));
             }}
           >
-            <div className="flex gap-1 items-center">
+            <div className="flex flex-col gap-1 items-center justify-center">
               {(button.value === "fish" || button.value === "bugs") && (
                 <img src={button.img} alt={button.text} className="w-10 h-10" />
               )}
